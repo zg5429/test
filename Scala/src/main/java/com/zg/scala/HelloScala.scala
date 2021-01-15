ad20c3d92a687a0427353e72220bc91690337ab4
zg0903 <zhuguangkr@gmail.com>
2021-01-15 20:54:28 +0800

    添加了语句

--- Scala/src/main/java/com/zg/scala/HelloScala.scala
+++ Scala/src/main/java/com/zg/scala/HelloScala.scala
@@ -8,40 +8,10 @@ package com.zg.scala
  */
 
 object HelloScala {
+
+
   def main(args: Array[String]): Unit = {
-    //    System.out.println("Hello Scala")
-    //    val name = "aa"
-    //    println("Hello Scala")
-    //    println(name)
-    var o = "\n"
-    println("aa" + o + "bb")
-
-    val a = new String("aaa")
-    val b = new String("aaa")
-    println(a == b)
-    println(a.equals(b))
-    println(a.eq(b))
-
-
-    for (i <- Range(1, 5); j <- Range(1, 4)) {
-      println("i = " + i + ",j = " + j)
-    }
-    println("---------------------")
-    for (i <- Range(1, 5)) {
-      for (j <- Range(1, 4)) {
-        println("i = " + i + ",j = " + j)
-      }
-
-    }
-
-
-    val result = for ( i <- Range(1,5) ) yield {
-      i * 2
-    }
-    println(result)
-
-
-
+    print("我是一颗菠菜")
   }
 
 }
